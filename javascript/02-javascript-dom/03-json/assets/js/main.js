ba1c760bf58011559b93d8e55a8e17aacef0c52c
Abi Thetchana <muthukumari.m28197@gmail.com>
2024-01-07 21:43:05 +0530

    fix: completed my json task

--- javascript/02-javascript-dom/03-json/assets/js/main.js
+++ javascript/02-javascript-dom/03-json/assets/js/main.js
@@ -1,4 +1,20 @@
 import months from "../data/main.json";
 
-console.log(months);
+const monthListEl = document.querySelector("#monthsList");
 
+months.forEach((month) => {
+  monthListEl.innerHTML += `<div class="li-month"> 
+  <div class="div-flex"><p class="month-list">Month:<p class="month-name">${month.month}</p></p> </div>
+  <div class="div-flex"><p class="month-list">Number of Days:<p class="month-name">${month.totaldays}</p></p> </div>
+  <div class="div-flex"><p class="month-list">Days: </p></div>
+  <div class="days"><p class="month-name">Sunday<p class="month-name">${month.days.sundays}</p></p></div>
+  <div class="days"><p class="month-name">Monday<p class="month-name">${month.days.monday}</p></p></div>
+  <div class="days"><p class="month-name">Tuesday<p class="month-name">${month.days.tuesday}</p></p></div>
+  <div class="days"><p class="month-name">Wednesday<p class="month-name">${month.days.wednesday}</p></p></div>
+  <div class="days"><p class="month-name">Thursday<p class="month-name">${month.days.thursday}</p></p></div>
+  <div class="days"><p class="month-name">Friday<p class="month-name">${month.days.friday}</p></p></div>
+  <div class="days"><p class="month-name">Saturday<p class="month-name">${month.days.saturday}</p></p></div>
+  <div class="div-flex"><p class="month-list">Holiday<p class="month-name">${month.holidayDate.join(", ")}</p></p> </div>
+  </div>
+  `;
+});
