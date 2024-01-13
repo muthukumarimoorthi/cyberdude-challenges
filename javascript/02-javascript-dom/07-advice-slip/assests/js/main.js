124bf1366f3ed2f5070249224ad5f5917ee66f29
Abi Thetchana <muthukumari.m28197@gmail.com>
2024-01-13 14:16:56 +0530

    fix: ui & ux

--- javascript/02-javascript-dom/07-advice-slip/assests/js/main.js
+++ javascript/02-javascript-dom/07-advice-slip/assests/js/main.js
@@ -1,22 +1,22 @@
 // https://api.adviceslip.com/advice
 
 const adviceBlockEl = document.querySelector("blockquote > p");
-const btnEl = document.getElementById("btn")
+const btnEl = document.getElementById("btn");
+const spanEl = document.getElementById("span");
 
 const apiURL = "https://api.adviceslip.com/advice";
 
-btnEl.style.display="none";
+btnEl.style.display = "none";
 
-btnEl.addEventListener("click", () =>{
-    window.location.href="/";
-})
+btnEl.addEventListener("click", () => {
+  window.location.href = "/";
+});
 
 const fetchAPI = fetch(apiURL)
   .then((response) => response.json())
   .then((data) => {
+    spanEl.innerText=`#${data.slip.id}`
     adviceBlockEl.innerText = `${data.slip.advice}`;
   });
-  
-  btnEl.style.display="block";
- 
-  
+
+btnEl.style.display = "block";
