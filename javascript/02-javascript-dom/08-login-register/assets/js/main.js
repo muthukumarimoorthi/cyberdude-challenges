9f42f82cfcee6152b9fbe44cd0fa558c0329f106
Abi Thetchana <muthukumari.m28197@gmail.com>
2024-01-19 23:32:46 +0530

    ui almost completed

--- javascript/02-javascript-dom/08-login-register/assets/js/main.js
+++ javascript/02-javascript-dom/08-login-register/assets/js/main.js
@@ -1,20 +1,35 @@
 import JustValidate from "just-validate";
 
 const registerFormEl = document.getElementById("registerForm");
-const btnEl = document.getElementById("btn");
+const loginFormEl = document.getElementById("loginForm");
+// console.log(loginFormEl);
+
+const validation = new JustValidate(registerFormEl);
+const validator = new JustValidate(loginFormEl);
+
+// console.log(validator);
+
+// Localkay init
 const localKey = "personData";
 
-const validator = new JustValidate(registerFormEl);
-// console.log(registerFormEl);
-
-validator.addField("#fullName", [
+// Full name validation
+validation.addField("#fullName", [
   {
     rule: "required",
     errorMessage: "Your name is required Ex: Abi Thetchana",
   },
+  {
+    rule: "minLength",
+    value: 3,
+  },
+  {
+    rule: "maxLength",
+    value: 25,
+  },
 ]);
 
-validator.addField("#email", [
+// Email validation
+validation.addField("#registerEmail", [
   {
     rule: "required",
     errorMessage: "Mail ID is required Ex: abithetchana@gmail.com",
@@ -23,72 +38,49 @@ validator.addField("#email", [
     rule: "email",
   },
 ]);
-validator.addField("#password", [
+
+// Password validation
+validation.addField("#registerPassword", [
   {
     rule: "required",
     errorMessage: "Password is required",
   },
+  {
+    rule: "password",
+  },
 ]);
-validator.addField("#confirm-password", [
+
+// Repeat password validation
+validation.addField("#confirm-password", [
   {
     rule: "required",
-    errorMessage: "Re-enter your password",
+    errorMessage: "Please re-enter password",
   },
   {
     validator: (value, fields) => {
-      if (fields["#password"].elem) {
-        const repeatPasswordValue = fields["#password"].elem.value;
-        // console.log(fields['#password'].elem.value);
+      if (fields["#registerPassword"] && fields["#registerPassword"].elem) {
+        const repeatPasswordValue = fields["#registerPassword"].elem.value;
+
         return value === repeatPasswordValue;
       }
+
+      return true;
     },
     errorMessage: "Passwords should be the same",
   },
 ]);
 
-validator.addField("#terms", [
+// Teams validation
+validation.addField("#terms", [
   {
     rule: "required",
     errorMessage: "You need to accept the terms and conditions",
   },
 ]);
 
-const API = "https://ipv4.icanhazip.com"
-const getIP = async () => {
-  const fetchURL = await fetch(API);
-  const bodyURL = await fetchURL.text()
-  return bodyURL;
-}
-
-const fetchData = await getIP()
-
-
-validator.onSuccess(() => {
+validation.onSuccess(() => {
   const formData = new FormData(registerFormEl);
-  formData.append("IP", fetchData);
   const formValueObj = Object.fromEntries(formData.entries());
-
-  
-  
-  // const getIpAddress = async ()=> {
-  //   try {
-  //     const response = await fetch(API);
-  //     if (response.ok) {
-  //       const ipAddress = await response.text();
-  //       console.log(ipAddress);
-  //       formData.append("IP", JSON.stringify(ipAddress))
-  //       // formData.append("IP", JSON.stringify(ipAddress));
-  //     } else {
-  //       console.log("COULDN'T GET YOUR IP.");
-  //     }
-  //   } catch (error) {
-  //     console.error("An error occurred:", error);
-  //   }
-  // }
-
-  // const edudaKambiya = await getIpAddress()
-  
-  // const localIP = await getIpAgetIpAddressddress()
 
   const existingData = localStorage.getItem("personData");
   const existingArray = JSON.parse(existingData);
@@ -104,8 +96,93 @@ validator.onSuccess(() => {
 
     localStorage.setItem(localKey, JSON.stringify(newData));
   }
-
   alert("submitted successfully!");
 
   registerFormEl.reset();
+});
+
+const localStorageValue = JSON.parse(localStorage.getItem("personData"));
+console.log(localStorageValue);
+
+validator.addField("#email", [
+  {
+    rule: "required",
+    errorMessage: "Mail ID is required Ex: abithetchana@gmail.com",
+  },
+]);
+
+validator.addField("#password", [
+  {
+    rule: "required",
+    errorMessage: "Password is required",
+  },
+  {
+    rule: "password",
+  },
+]);
+
+validator.addField("#remember", [
+  {
+    rule: "required",
+    errorMessage: "Please select the field",
+  },
+]);
+
+validator.onSuccess(() => {
+  const emailEl = document.getElementById("email").value;
+  const passwordEl = document.getElementById("password").value;
+
+  const findUserData = localStorageValue.find((value) => {
+    // console.log(value);
+    return (
+      value.registerEmail == emailEl && value.registerPassword == passwordEl
+    );
+  });
+
+  const loginSectionHiddenDivEl = document.getElementById(
+    "loginSectionHiddenDiv"
+  );
+  const noteSectionEl = document.getElementById("noteSection");
+  if (findUserData) {
+    loginSectionHiddenDivEl.classList.replace("block", "hidden");
+    noteSectionEl.classList.replace("hidden", "block");
+  } else {
+    alert("Incorrect username and password! Please check your value!");
+    loginFormEl.reset();
+  }
+});
+
+const loginHereEl = document.getElementById("loginHere");
+// console.log(loginHereEl);
+const registerNav = document.getElementById("registerSection");
+const loginSectionHiddenEl = document.getElementById("loginSectionHidden");
+
+loginHereEl.addEventListener("click", () => {
+  loginSectionHiddenEl.classList.add("hidden", "block");
+  registerNav.classList.add("hidden");
+});
+
+const signUpEl = document.getElementById("signUp");
+// console.log(signUpEl);
+
+signUpEl.addEventListener("click", () => {
+  loginSectionHiddenEl.classList.add("hidden");
+  registerNav.classList.replace("hidden", "block");
+});
+
+const API = "https://ipv4.icanhazip.com";
+const getIP = async () => {
+  const fetchURL = await fetch(API);
+  const bodyURL = await fetchURL.text();
+  return bodyURL;
+};
+
+const fetchData = await getIP();
+// console.log(fetchData);
+
+const ipSectionSpanEl = document.getElementById("ipSectionSpan");
+const clickHereBtn = document.getElementById("clickHere");
+
+clickHereBtn.addEventListener("click", () => {
+  ipSectionSpanEl.innerText = fetchData;
 });
