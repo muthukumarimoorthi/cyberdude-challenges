14987fce5df08bcfa0d78198e43bbc7497804b44
Abi Thetchana <muthukumari.m28197@gmail.com>
2024-01-30 13:55:35 +0530

    person1 completed

--- javascript/03-react/01-react-basic/src/persons/PersonOne.jsx
+++ javascript/03-react/01-react-basic/src/persons/PersonOne.jsx
@@ -1,13 +1,187 @@
 import React from "react";
 
-const PersonOne = ()=>{
-    return(
-        <section>
-            <div className="max-w-2xl mx-auto bg-slate-700 rounded my-10">
-            hi
+const PersonOne = () => {
+  return (
+    <section>
+      <div className="max-w-2xl mx-auto bg-slate-700 rounded my-10 px-5 py-5">
+        <div className="flex item-center justify-between py-5">
+          <div className="flex items-center space-x-3">
+            <div>
+              <img
+                src="https://media.licdn.com/dms/image/D5635AQG-1qz6lthNKQ/profile-framedphoto-shrink_400_400/0/1705918214896?e=1707199200&v=beta&t=A6UlFBCHBLdbkxWeE_PXA5sIayXLMMR4YtEVIqMP9uQ"
+                alt="muthukumari.img"
+                className="w-16 rounded-full"
+              />
             </div>
-        </section>
-    )
-}
+            <div className="tracking-wider flex flex-col">
+              <div className="flex">
+                <div className="flex flex-col">
+                  <span className="text-slate-100 font-normal text-base">
+                    Muthukumari M{" "}
+                  </span>
+                  <span className="text-slate-400 text-xs">
+                    Full Stack Engineer
+                  </span>
+                  <div className="flex items-center">
+                    <span className="text-slate-400 text-xs">6d</span>
+                    <span className="text-slate-400">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32"
+                        height="32"
+                        viewBox="0 0 256 256"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M144 128a16 16 0 1 1-16-16a16 16 0 0 1 16 16"
+                        />
+                      </svg>
+                    </span>
+                    <span className="text-slate-300">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="15"
+                        height="15"
+                        viewBox="0 0 32 32"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M16 2a14 14 0 1 0 14 14A14.016 14.016 0 0 0 16 2M4 16a11.915 11.915 0 0 1 .7-4H10l5 4l-2.8 3.693A1 1 0 0 0 12.293 21L15 24v3.95A12.01 12.01 0 0 1 4 16m17.435 10.685l2.546-7.7a1 1 0 0 0-.367-.985L15 11l2-2h5.28a1 1 0 0 0 .948-.684l.495-1.486a11.974 11.974 0 0 1-2.288 19.855"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+                <span className="text-slate-400"> (She/Her)</span>
+                <span className="text-slate-400">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 256 256"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M144 128a16 16 0 1 1-16-16a16 16 0 0 1 16 16"
+                    />
+                  </svg>
+                </span>
+                <span className="text-slate-400"> You</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <span className="text-slate-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 12a1 1 0 1 0 2 0a1 1 0 1 0-2 0m7 0a1 1 0 1 0 2 0a1 1 0 1 0-2 0m7 0a1 1 0 1 0 2 0a1 1 0 1 0-2 0"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-y-5">
+          <span className="text-sm text-slate-200">
+            {" "}
+            🚀 Project # 7 – Google Meet UI Clone Challenge With Tailwind CSS
+          </span>
+          <span className="text-sm text-blue-500 space-x-1">
+           <a href="https://www.linkedin.com/company/cyberdude-networks/" target="blank" className="hover:underline">#cyberdude_internship </a> <a href="https://www.linkedin.com/company/cyberdude-networks/" target="blank" className="hover:underline">#cyberdude </a>
+           <a href="" target="blank" className="hover:underline">#fullstack_engineer </a>
+          </span>
+          <span className="text-sm text-slate-200">🖊 Project description</span>
+          <span className="text-sm text-slate-200">
+            I was assigned with the project by <span className=" text-blue-500 hover:underline"><a href="https://www.linkedin.com/company/cyberdude-networks/">CyberDude Networks Private
+            Limited</a></span> to create a Google Meet UI by using the simple HTML and
+            Tailwind CSS. The Project is to create a build a lookalike Google
+            Meet UI. (This is also Mobile responsive). This was the first UI
+            challenge assigned to me by Mentor
+            <span className=" text-blue-500 hover:underline"><a href="https://www.linkedin.com/in/anburocky3/"> Anbuselvan Annamalai</a></span>
+          </span>
+          <span className="text-sm text-slate-200">⌨ Features</span>
+          <span className="text-sm text-slate-200">
+            - Clean UI based on previous version of Google Meet UI.
+          </span>
+          <span className="text-sm text-slate-200">💪🏽 Challenges</span>
+          <div className="flex flex-col space-y-2">
+            <span className="text-sm text-slate-200">
+              - This is my Fourth project using the Tailwind CSS. It was easier
+              than before for me to complete this task, as I was familiar with
+              the Tailwind CSS because of my previous projects.
+            </span>
+            <span className="text-sm text-slate-200">
+              - This is where I learned the core concept of the Tailwind CSS. I
+              learned where I need to use Tailwind CSS and where to use the Grid
+              Flex.
+            </span>
+            <span className="text-sm text-slate-200">
+              - This was a special project to me. It’s because I built it from
+              scratch. I did not get any help, I handled all the work.
+            </span>
+            <span className="text-sm text-slate-200">
+              - Until now my mentor use to give me guidelines of the project.
+              But this is the first project I completed without any help.
+            </span>
+            <span className="text-sm text-slate-200">
+              - I know the concept of the Grid. But this is where I learned how
+              does the Grid work. I have worked with Grid previously, But this
+              is where I under the working of Grid.
+            </span>
+            <span className="text-sm text-slate-200">
+              - After completion of this project I was more confident I will
+              able to create an UI by myself.
+            </span>
+            <span className="text-sm text-slate-200">
+              - As a Amateur I feel confident after the completion of this
+              project. Now, I knew and having a clear understand at how Tailwind
+              work with HTML.
+            </span>
+          </div>
+          <span className="text-sm text-slate-200">Unlocked skills during this project - <span className="text-blue-500 space-x-1">
+            <a href="#" className="hover:underline">#TailwindCSS</a> 
+            <a href="" className="hover:underline">#CSS_Positioning</a> 
+            <a href="" className="hover:underline">#Vite</a> 
+            <a href="" className="hover:underline">#Gmeet</a></span></span>
+          <div className="flex flex-col">
+          <span className="text-sm text-slate-200">⚡GitHub Repo : <span className="text-blue-500 hover:underline"><a href="https://github.com/muthukumarimoorthi/gmeet-clone-challenge" target="blank">https://lnkd.in/g7S_ZqE3</a></span></span>
+          <span className="text-sm text-slate-200">⚡GitHub Live : <a href="https://muthukumarimoorthi.github.io/gmeet-clone-challenge/" className="text-blue-500 hover:underline" target="blank">https://lnkd.in/gjUH_GjG</a></span>
+          <span className="text-sm text-slate-200">⚡GitHub profile : <a href="https://github.com/muthukumarimoorthi" className="text-blue-500 hover:underline" target="blank">https://lnkd.in/gr_WfbN5</a></span>
+
+          </div>
+          <div className="flex flex-col">
+          <span className="text-sm text-slate-200">I am very thankful to Anbuselvan Annamalai (My Mentor).</span>
+          <span className="text-sm text-slate-200">Special thanks to Muhammed Shajid for helping me with this Project.</span>
+          <span className="text-sm text-slate-200">Special thanks to Vijayavedhasekaran K for helping me with making the UI responsive.</span>
+          </div>
+          
+          <span className="text-sm text-slate-200">Please share your thoughts on this in the comment section or DM. Your thoughts and feedbacks are much appreciated.</span> 
+         
+  
+          <a href="https://lnkd.in/gr_WfbN5"><img src="https://i.ibb.co/SfbPtV4/Screenshot-2023-10-14-162215.png" alt="Screenshot-2023-10-14-162215" border="0"/></a>
+
+         <div className="flex justify-between items-center">
+         <div className="flex space-x-3">
+          <div className="flex"><img src="https://www.userflow.nl/images/Linkedin-Support-Icon-HeartinHand500.png" className="w-5 h-5" alt="like" />
+          <img src="https://www.userflow.nl/images/Linkedin-Love-Icon-Heart500.png" className="w-5 h-5" alt="love" /></div>
+          <span className="text-xs text-slate-300"> Vijayavedhasekaran K adnd 46 others</span>
+          </div>
+          <div> <span className="text-xs text-slate-300">6 comments</span></div>
+         </div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default PersonOne;
