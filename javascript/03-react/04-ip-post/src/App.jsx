6232309e4ceeebb322d6393f36b001a6101bb098
Abi Thetchana <muthukumari.m28197@gmail.com>
2024-02-02 15:30:14 +0530

    fetched api and get posts

--- javascript/03-react/04-ip-post/src/App.jsx
+++ javascript/03-react/04-ip-post/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const App = ()=>{
-    useState()
-    return (
-        <div className="bg-red-500 text-2xl">
+const App = () => {
+  const [post, setPost] = useState([]);
 
-        </div>
-    )
-}
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const fetchedPosts = await fetch("https://dummyjson.com/posts");
+      const postJson = await fetchedPosts.json();
+      console.log(postJson);
+    };
+    fetchPosts();
+  }, []);
+
+  return <div className="bg-red-500 text-2xl"></div>;
+};
 
 export default App;
