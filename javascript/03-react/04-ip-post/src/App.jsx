335301b6281d0760559d8c3e3f34d510fb94b918
Abi Thetchana <muthukumari.m28197@gmail.com>
2024-02-02 14:31:34 +0530

    init

---
+++ javascript/03-react/04-ip-post/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const App = ()=>{
+    useState()
+    return (
+        <div className="bg-red-500 text-2xl">
+
+        </div>
+    )
+}
+
+export default App;
