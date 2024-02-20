eeb4d6a446b7f8626facdd59457be7bbf9f931a3
Abi Thetchana <muthukumari.m28197@gmail.com>
2024-02-21 00:13:02 +0530

    completed creating file task

--- javascript/04-node-js/01-creating-file-system/index.js
+++ javascript/04-node-js/01-creating-file-system/index.js
@@ -1,4 +1,4 @@
-const fs =require('fs');
+const fs = require("fs");
 
 //1) Create a json file with the places you wanna to visit ✅
 //2) creating a folder named places ✅
@@ -16,12 +16,20 @@ const myFvtCities = require("./data/myFvtCities.json");
 
 // console.log(myFvtCities);
 
-myFvtCities.map((location)=>{
-    // console.log(places)
-    fs.mkdir(`places/${location.id} ) ${location.location}`,(error)=>{error ? console.log(error) : console.log("folder created successfully")})
-    location.placesName.map((places)=>{
-        fs.writeFile(`places/${location.id} ) ${location.location}/${places.placeName}.txt`,`${places.about}`,()=>{})
-    })
+myFvtCities.map((location) => {
+  console.log(location);
+
+  fs.mkdir(`places/0${location.id}-${location.location.toLocaleLowerCase()}`, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("File written successfully!..");
+  });
+
+  location.myFavouritePlaces.map((place)=>{
+    fs.writeFile(`places/0${location.id}-${location.location.toLocaleLowerCase()}/${place.placeName.toLocaleLowerCase()}.txt`, `${place.about}`, ()=>{} )
+  })
 
 
-})
+});
